633fe70153449c3c1ca1f1093d925947fec2cd6d
larisa.pomidor <nikakinikakin@gmail.com>
2023-12-20 17:07:28 +0200

    Tasks added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 fun findMissingAndRepeatedValues(grid: Array<IntArray>): IntArray {
     val flattenedArray = grid.flatMap { it.asIterable() }
     val occurrenceMap = flattenedArray.groupingBy { it }.eachCount()
@@ -23,6 +25,52 @@ fun countTestedDevices(batteryPercentages: IntArray): Int {
     return result
 }
 
+fun findIntersectionValues(nums1: IntArray, nums2: IntArray): IntArray {
+    val result: IntArray = intArrayOf(0, 0)
+
+    nums1.forEach outer@ { element1 ->
+        nums2.forEach inner@ { element2 ->
+            if (element1 == element2) {
+                result[0]++
+                return@outer
+            }
+        }
+    }
+
+    nums2.forEach outer@ { element1 ->
+        nums1.forEach inner@ { element2 ->
+            if (element1 == element2) {
+                result[1]++
+                return@outer
+            }
+        }
+    }
+
+    return result
+}
+
+fun findPeaks(mountain: IntArray): List<Int> {
+    val result: MutableList<Int> = mutableListOf()
+
+    for (i in mountain.indices) {
+        if (i > 0 && i < mountain.size - 1
+            && (mountain[i - 1] < mountain[i] && mountain[i] > mountain[i + 1])) {
+            result.add(i)
+        }
+    }
+    return result
+}
+
+fun areSimilar(mat: Array<IntArray>, k: Int): Boolean {
+    for (i in mat.indices) {
+        for (j in mat[i].indices) {
+                if (mat[i][j] != mat[i][(j + abs((i % 2 - 1) * k)) % k])
+                    return false
+        }
+    }
+    return true
+}
+
 fun main() {
     val grid = arrayOf(
         intArrayOf(9, 1, 7),
@@ -30,6 +78,7 @@ fun main() {
         intArrayOf(3, 4, 6)
     )
 
-    findMissingAndRepeatedValues(grid)
-    countTestedDevices(intArrayOf(1, 1, 2, 1, 3))
+    //findMissingAndRepeatedValues(grid)
+    //countTestedDevices(intArrayOf(1, 1, 2, 1, 3))
+    findIntersectionValues(intArrayOf(4,3,2,3,1), intArrayOf(2,2,5,2,3,6))
 }
