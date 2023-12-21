aeecaba33baae1d6a9ddaad426f48365b20883a4
larisa.pomidor <nikakinikakin@gmail.com>
2023-12-21 11:14:42 +0200

    Tasks added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -60,15 +60,33 @@ fun findPeaks(mountain: IntArray): List<Int> {
     }
     return result
 }
-
 fun areSimilar(mat: Array<IntArray>, k: Int): Boolean {
     for (i in mat.indices) {
         for (j in mat[i].indices) {
-                if (mat[i][j] != mat[i][(j + abs((i % 2 - 1) * k)) % k])
-                    return false
+            if (mat[i][j] != mat[i][(j + abs((1 - 2 * (i % 2)) * k)) % mat[i].size])
+                return false
         }
     }
     return true
+}
+
+fun findWordsContaining(words: Array<String>, x: Char): List<Int> {
+    val result: MutableList<Int> = mutableListOf()
+    words.forEachIndexed { index, word ->
+        if (word.contains(x)) result.add(index)
+    }
+    return result
+}
+
+fun findMinimumOperations(s1: String, s2: String, s3: String): Int {
+    var length = 0
+    while (s1.length >= length + 1 && s2.length >= length + 1 && s3.length >= length + 1) {
+        if (s1[length] == s2[length] && s1[length] == s3[length] && s2[length] == s3[length]) {
+            length++
+        }
+        else break
+    }
+    return if (length != 0) s1.length + s2.length + s3.length - length * 3 else -1
 }
 
 fun main() {
@@ -80,5 +98,13 @@ fun main() {
 
     //findMissingAndRepeatedValues(grid)
     //countTestedDevices(intArrayOf(1, 1, 2, 1, 3))
-    findIntersectionValues(intArrayOf(4,3,2,3,1), intArrayOf(2,2,5,2,3,6))
+    //findIntersectionValues(intArrayOf(4,3,2,3,1), intArrayOf(2,2,5,2,3,6))
+
+    val mat = arrayOf(
+        intArrayOf(2, 2),
+        intArrayOf(25, 23)
+    )
+
+    //areSimilar(mat, 1)
+    findMinimumOperations("abc","abb","ab")
 }
